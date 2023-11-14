f2794ec0f86d06421729a91f6516598a49062001
India <India@India-PC>
2023-11-14 22:06:59 +0530

    m

---
+++ main.tf
@@ -0,0 +1,57 @@
+	variable "components" {
+	  default = [
+	    "frontend",
+	    "cart",
+	    "catalogue",
+	    "user",
+	    "payment",
+	    "shipping",
+	    "dispatch",
+	    "mysql",
+	    "rabbitmq",
+	    "redis",
+	    "mongodb",
+	  ]
+	}
+	
+	
+	data "aws_ami" "ami" {
+	  most_recent = true
+	  name_regex  = "Centos-8-DevOps-Practice"
+	  owners      = ["973714476881"]
+	}
+	
+	
+	
+	resource "aws_instance" "instance" {
+	  count = length(var.components)
+	  ami = data.aws_ami.ami.id
+	  instance_type = "t3.micro"
+	  vpc_security_group_ids = ["sg-0d9ac6aeffed99dc2"]
+	  tags = {
+	    Name = element(var.components, count.index)
+	  }
+	}
+	
+	resource "aws_route53_record" "record" {
+	  count = length(var.components)
+	  zone_id = "Z09059901XRPHNYMGLMJ4"
+	  name    = "${element(var.components, count.index)}-dev"
+	  type    = "A"
+	  ttl     = 30
+	  records = [element(aws_instance.instance.*.private_ip, count.index)]
+	}
+	
+	resource "null_resource" "set-hostname" {
+	  count = length(var.components)
+	  provisioner "remote-exec" {
+	    connection {
+	      host = element(aws_instance.instance.*.private_ip, count.index)
+	      user = "root"
+	      password = "DevOps321"
+	    }
+	    inline = [
+	      "set-hostname -skip-apply ${var.components[count.index]}"
+	    ]
+	  }
+	}
